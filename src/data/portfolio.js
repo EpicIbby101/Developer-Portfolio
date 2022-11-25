c460c32a6aef41022b5f683ffec6e68111da88e0
Kevin Grittner <Kevin_grittner@outlook.com>
2022-11-25 01:02:14 +0000

    Initial Commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Placeholder',
+        imgUrl: 'assets/clouds.JPG',
+        stack: ['HTML', 'CSS', 'JavaScript'],
+        link: 'https://github.com/EpicIbby101',
+    },
+    {
+        title: 'Placeholder',
+        imgUrl: 'assets/earth.jpg',
+        stack: ['HTML', 'CSS', 'JavaScript'],
+        link: 'https://github.com/EpicIbby101',
+    },
+    {
+        title: 'Placeholder',
+        imgUrl: 'assets/cybercity.png',
+        stack: ['HTML', 'CSS', 'JavaScript'],
+        link: 'https://github.com/EpicIbby101',
+    },
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        year: '2022',
+        title: 'Began Studying Computer Science',
+        duration: '3 months',
+        details: 
+            'I have dabbled in code a bit before, but only started seriously studying to become a software engineer since about September, investing all my time and efforts into this. Since then I have learnt the essentials of both Front End and Back End Web Development.',
+    },
+    {
+        year: '2021',
+        title: 'Administrator/ Support Assistant and Content Creator',
+        duration: '1 Year',
+        details: 'I was responsible for designing the content of the systems that were shown on TVs in hotels, hospitals and other healthcare related facilities as well as configure them before installation.',
+    },
+    {
+        year: '2020',
+        title: 'Small time Photo/ video Editor',
+        duration: '2 years',
+        details: 'At the height of Covid, while stuck indoors I took up a course in Photo and Video Editing, mastering programs like Photoshop and Premiere Pro. This helped me land my future role at a small B2B company creating content for the TV systems used in the Hospitality industry.',
+    },
+    {
+        year: '2015',
+        title: 'PC Builder',
+        duration: '7 years',
+        details: 'Took up a hobby in PC Gaming while in school and built my first PC. Impressed friends who also wanted to get into PC Gaming looked to me to build and install PCs for them. I have gained familiarity with computer hardware and was the main go-to person for PC diagnostics among classmates and friends.',
+    },
+]
