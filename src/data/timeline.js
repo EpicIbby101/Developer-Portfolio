3090a19d8dd99dec0405c348825b17fd5829f624
EpicIbby101 <kevin_grittner@outlook.com>
2023-01-01 01:12:33 +0000

    Added timeline items

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -4,24 +4,24 @@ export default [
         title: 'Began Studying Computer Science',
         duration: '3 months',
         details: 
-            'I have dabbled in code a bit before, but only started seriously studying to become a software engineer since about September, investing all my time and efforts into this. Since then I have learnt the essentials of both Front End and Back End Web Development.',
+            'I have dabbled in code a bit before, but only started seriously studying to become a software engineer since about September 2022, investing all my time and efforts into learning the essentials of both front and back end development',
     },
     {
         year: '2021',
         title: 'Administrator/ Support Assistant and Content Creator',
         duration: '1 Year',
-        details: 'I was responsible for designing the content of the systems that were shown on TVs in hotels, hospitals and other healthcare related facilities as well as configure them before installation.',
+        details: 'I was responsible for designing the content of the systems that were shown on TVs in the hospitality and healthcare industry as well as configure them before installation. I have received Administrative, I.T related and engineering experience from this job.',
     },
     {
         year: '2020',
         title: 'Small time Photo/ video Editor',
         duration: '2 years',
-        details: 'At the height of Covid, while stuck indoors I took up a course in Photo and Video Editing, mastering programs like Photoshop and Premiere Pro. This helped me land my future role at a small B2B company creating content for the TV systems used in the Hospitality industry.',
+        details: 'At the height of Covid, while stuck indoors I took up photo and video editing, mastering programs like Photoshop and Premiere Pro. This helped me land my future role at a small B2B company creating content for the TV systems used in the Hospitality/ Healthcare industry.',
     },
     {
         year: '2015',
         title: 'PC Builder',
         duration: '7 years',
-        details: 'Took up a hobby in PC Gaming while in school and built my first PC. Impressed friends who also wanted to get into PC Gaming looked to me to build and install PCs for them. I have gained familiarity with computer hardware and was the main go-to person for PC diagnostics among classmates and friends.',
+        details: 'Took up a hobby in PC Gaming while in school and built my first PC. Impressed friends who also wanted to get into PC Gaming looked to me to build and install PCs for them. I have gained familiarity with computer hardware and was the main go-to person for PC diagnostics/ expertise among classmates and friends.',
     },
 ]
