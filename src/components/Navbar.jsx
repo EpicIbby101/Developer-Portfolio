9aba42f51b1094f724f6172ace6d3f724c8de884
EpicIbby101 <kevin_grittner@outlook.com>
2023-01-01 00:46:40 +0000

    Fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,14 @@ const Navbar = () => {
   return (
   
     <div className="fixed flex justify-between items-center h-16 sm:h-16 md:h-16 z-10 bg-white/80 dark:bg-gray-700/80 backdrop-blur w-full shadow-md transition duration-500">
-      <span className="relative m-7 z-30 font-bold text-3xl">Welcome to my Portfolio</span>
+      <span className="relative m-7 z-30 font-bold text-3xl">My Portfolio</span>
       <div className=" flex items-center mr-20">
       <a
                 href="https://github.com/EpicIbby101"
                 target="_blank">
                 <img 
                 src="assets/Githublogo.png"
-                className="w-11 ml-2"
+                className="w-11 md:w-11 ml-2"
                 ></img>
                 </a>
 
@@ -21,7 +21,7 @@ const Navbar = () => {
                 target="_blank">
                 <img 
                 src="assets/linkedin.png"
-                className="w-10 ml-3"
+                className="w-10 md:w-11 ml-3"
                 ></img>
                 </a>
 
@@ -30,7 +30,7 @@ const Navbar = () => {
                 target="_blank">
                 <img 
                 src="assets/instagram.svg"
-                className="w-10 ml-3 "
+                className="w-10 md:w-11 ml-3 "
                 ></img>
                 </a>
                 <p className="ml-5">||</p>
