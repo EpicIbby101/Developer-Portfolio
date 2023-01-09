7d8f34fd8bb97e8c0a95acd2eb62818133398add
EpicIbby101 <kevin_grittner@outlook.com>
2023-01-09 17:21:29 +0000

    Added docker to tools

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -124,6 +124,15 @@ function Intro() {
             height="40"
           />{" "}
         </a>
+        <a href="https://www.docker.com/" target="_blank" rel="noreferrer">
+          {" "}
+          <img
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg"
+            alt="docker"
+            width="40"
+            height="40"
+          />{" "}
+        </a>
         <a href="https://firebase.google.com/" target="_blank" rel="noreferrer">
           {" "}
           <img
