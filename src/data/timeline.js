70119111e3395936da450ba1146af7bf02d0a22b
EpicIbby101 <kevin_grittner@outlook.com>
2023-03-21 21:30:48 +0000

    Updated timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,27 +1,27 @@
 export default [
     {
-        year: '2022',
+        year: '2022 - Present',
         title: 'Began Studying Computer Science',
-        duration: '6 months',
+        duration: '8 months',
         details: 
-            'I have dabbled in code a bit before, but only started seriously studying to become a software engineer since about July 2022, investing all my time and efforts into learning the essentials of both front and back end development',
+            'After a short holiday and a lengthy conversation with a friend who is currently already a great software engineer, I decided to dedicate all of my time into studying Full-Stack Web Development. It was a rocky road to be sure but by December I had become increasingly proficient in both frontend and backend technologies; building a solid foundation in JavaScript, React and NodeJS as well as HTML and CSS. From there it was all projects and furthering my understanding of the tech industry. I am currently looking for my next opportunity to shine and perhaps lend my experience to a great team in the professional world',
     },
     {
-        year: '2021',
+        year: '2021 - 2022',
         title: 'Administrator/ Support Assistant and Content Creator',
         duration: '1 Year',
-        details: 'I was responsible for designing the content of the systems that were shown on TVs in the hospitality and healthcare industry as well as configure them before installation. I have received Administrative, I.T related and engineering experience from this job.',
+        details: 'I was responsible for elevating our company website with collaborative Wordpress and PHP updates, increasing SEO visibility and user experience. I also helped design a lot of content for the systems that were shown on TVs in the hospitality and healthcare industry as well as configure them before installation. Lastly I aided in boosting revenue for established businesses by 20% through creative marketing strategies and targeted sales. It was at this job that I started to slowly get into tech and programming, but I would not invest a lot of time into it until I left the company.'
     },
     {
-        year: '2020',
-        title: 'Small time Photo/ video Editor',
-        duration: '2 years',
-        details: 'At the height of Covid, while stuck indoors I took up photo and video editing, mastering programs like Photoshop and Premiere Pro. This helped me land my future role at a small B2B company creating content for the TV systems used in the Hospitality/ Healthcare industry.',
+        year: '2018 - 2021',
+        title: 'Quality Inspector',
+        duration: '3 Years',
+        details: 'I started as a simple Machine Operator manufacturing and assembling automotive components for JLR, I was exponentially skilled at my job and was able to rotate around 5 machines to keep their tickets down. I worked my way up to a Quality Inspector Role, and under my leadership and contributions I became responsible for boosting team productivity by 60% efficiency by 45% and work quality by 85% while reducing defective products and saving valuable rework hours.'
     },
     {
         year: '2015',
         title: 'PC Builder',
-        duration: '7 years',
-        details: 'Took up a hobby in PC Gaming while in school and built my first PC. Impressed friends who also wanted to get into PC Gaming looked to me to build and install PCs for them. I have gained familiarity with computer hardware and was the main go-to person for PC diagnostics/ expertise among classmates and friends.',
+        duration: '8 years',
+        details: 'Took up a hobby in PC Gaming while in school and built my first computer. Impressed friends who also wanted to get into PC Gaming looked to me to build and install PCs for them. I have gained familiarity with computer hardware and software and was the main go-to person for PC diagnostics/ expertise among classmates and friends. To this day I remain a loyal and reliable friend for those who are having issues with their devices.',
     },
 ]
