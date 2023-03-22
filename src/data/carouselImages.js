d8bf31b46e82131f7a56a3ec157dc72ea6308c68
EpicIbby101 <kevin_grittner@outlook.com>
2023-03-22 01:21:02 +0000

    cleaned more code

--- src/data/carouselImages.js
+++ src/data/carouselImages.js
@@ -1,10 +1,10 @@
 export const images = [
-  ".assets/Neon Yang.png",
-  ".assets/nawaf.png",
-  ".assets/pnl.png",
-  ".assets/Melancholy Darker.png",
-  ".assets/moon.jpg",
-  ".assets/Space Jellies.jpg",
-  ".assets/Temple Finalised.jpg",
+  "./assets/Neon Yang.png",
+  "./assets/nawaf.png",
+  "./assets/pnl.png",
+  "./assets/Melancholy Darker.png",
+  "./assets/moon.jpg",
+  "./assets/Space Jellies.jpg",
+  "./assets/Temple Finalised.jpg",
   "./assets/Ibby2.png"
 ];
